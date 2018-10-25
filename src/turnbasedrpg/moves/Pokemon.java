80a15fa9265a9f3e01f1abcc5c63048c1cd2915b
Matheus Oliveira <matheus.oliveira@softexpert.com>
2018-10-25 13:31:19 -0300

    Stats WIP

--- src/turnbasedrpg/moves/Pokemon.java
+++ src/turnbasedrpg/moves/Pokemon.java
@@ -2,6 +2,7 @@ package turnbasedrpg.moves;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Random;
 
 /**
  *
@@ -9,22 +10,85 @@ import java.util.Arrays;
  */
 public class Pokemon {
 
-    String name = "";
     int level = 1;
+    int[] physicalAttack;
+    int[] specialAttack;
+    int[] physicalDefense;
+    int[] specialDefense;
+    int[] speed;
+    int[] health;
+    
+    int physicalAttack;
+    int specialAttack;
+    int physicalDefense;
+    int specialDefense;
+    int speed;
+    int health;
+
+    int physicalAttackIV;
+    int specialAttackIV;
+    int physicalDefenseIV;
+    int specialDefenseIV;
+    int speedIV;
+    int healthIV;
+
+    String name = "";
     Moves[] moves;
-    
-    public Pokemon () {
+
+    public Pokemon() {
+        Random rand = new Random();
+
+        // Gera IVs aleatórios
+        physicalAttackIV = rand.nextInt(31);
+        specialAttackIV = rand.nextInt(31);
+        physicalDefenseIV = rand.nextInt(31);
+        specialDefenseIV = rand.nextInt(31);
+        speedIV = rand.nextInt(31);
+        healthIV = rand.nextInt(31);
     }
-    
+
+    public int getStatValue(int stat, int IV, int isHealth) {
+        return (((IV + 2 * stat) * level / 100) + 10 + level);
+    }
+
+    public int getStatValue(int stat, int IV) {
+        return (((IV + 2 * stat) * level / 100) + 5); // * Nature Value
+    }
+
+    public int getHealthValue() {
+        return this.getStatValue(health, healthIV, 1);
+    }
+
+    public int getPhysicalAttackValue() {
+        return this.getStatValue(physicalAttack, physicalAttackIV);
+    }
+
+    public int getSpecialAttackValue() {
+        return this.getStatValue(specialAttack, specialAttackIV);
+    }
+
+    public int getPhysicalDefenseValue() {
+        return this.getStatValue(physicalDefense, physicalDefenseIV);
+    }
+
+    public int getSpecialDefenseValue() {
+        return this.getStatValue(specialDefense, specialDefenseIV);
+    }
+
+    public int getSpeedValue() {
+        return this.getStatValue(speed, speedIV);
+    }
+
     public void createPokemon(String name, Moves[] moves) {
         this.name = name;
         this.moves = moves;
+        this.level = 5;
     }
-    
+
     public Moves getPokemonMove(int arrayPosition) {
         return this.moves[arrayPosition];
     }
-    
+
     public Pokemon getSquirtle() {
         Moves[] movesArray = new Moves[4];
         movesArray[0] = new Moves().getNormalMove(1);
@@ -34,7 +98,7 @@ public class Pokemon {
         this.createPokemon("Squirtle", movesArray);
         return this;
     }
-    
+
     public String getName() {
         return name;
     }
