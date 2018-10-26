87e47c344584bcc4b751027d75b8d0f2afc46258
Matheus Oliveira <matheus.oliveira@softexpert.com>
2018-10-26 13:26:30 -0300

    Damage calc 1.0

--- src/turnbasedrpg/moves/Combat.java
+++ src/turnbasedrpg/moves/Combat.java
@@ -5,10 +5,34 @@
  */
 package turnbasedrpg.moves;
 
+import java.util.Random;
+
 /**
  *
  * @author matheus.oliveira
  */
 public class Combat {
+    Pokemon player1;
+    Pokemon player2;
     
+    public Combat(Pokemon pokemon1, Pokemon pokemon2) {
+        player1 = pokemon1;
+        player2 = pokemon2;
+    }
+    
+    public void calculateDamage(int moveID, int player) {
+        Pokemon attacker = (player == 1) ? player1 : player2;
+        Pokemon defender = (player == 1) ? player2 : player1;
+        Moves move = attacker.getPokemonMove(moveID);
+        
+        Random rand = new Random();
+        int randomNumber = rand.nextInt(16) + 85;
+
+        //  Fórmula de calculo de dano: Damage = ((((2 * Level / 5 + 2) * AttackStat * AttackPower / DefenseStat) / 50) + 2) * STAB * Weakness/Resistance * RandomNumber / 100
+        int category = move.getFgCategory();
+        int damage = ((((2 * attacker.getLevel() / 5 + 2) * attacker.getAttackValue(category) * move.getPower() / defender.getDefenseValue(category)) / 50) + 2) 
+                /** STAB * Weakness/Resistance * */ * randomNumber / 100;
+
+        
+    }
 }
--- src/turnbasedrpg/moves/Pokemon.java
+++ src/turnbasedrpg/moves/Pokemon.java
@@ -1,82 +1,55 @@
 package turnbasedrpg.moves;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.Random;
 
 /**
  *
  * @author matheus.oliveira
  */
-public class Pokemon {
+public final class Pokemon {
 
     int level = 1;
+
     int[] physicalAttack;
     int[] specialAttack;
     int[] physicalDefense;
     int[] specialDefense;
     int[] speed;
     int[] health;
-    
-    int physicalAttack;
-    int specialAttack;
-    int physicalDefense;
-    int specialDefense;
-    int speed;
-    int health;
-
-    int physicalAttackIV;
-    int specialAttackIV;
-    int physicalDefenseIV;
-    int specialDefenseIV;
-    int speedIV;
-    int healthIV;
 
     String name = "";
     Moves[] moves;
 
     public Pokemon() {
         Random rand = new Random();
+        // Configura o nível
+        setLevel(50);
 
         // Gera IVs aleatórios
-        physicalAttackIV = rand.nextInt(31);
-        specialAttackIV = rand.nextInt(31);
-        physicalDefenseIV = rand.nextInt(31);
-        specialDefenseIV = rand.nextInt(31);
-        speedIV = rand.nextInt(31);
-        healthIV = rand.nextInt(31);
+        setPhysicalAttackIV(rand.nextInt(31));
+        setSpecialAttackIV(rand.nextInt(31));
+        setPhysicalDefenseIV(rand.nextInt(31));
+        setSpecialDefenseIV(rand.nextInt(31));
+        setSpeedIV(rand.nextInt(31));
+        setHealthIV(rand.nextInt(31));
+
+        // Calcula os valores dos stats
+        setHealthValue(calculateStatValue(getHealthStat(), getHealthIV(), 1));
+        setSpeedValue(calculateStatValue(getSpeedStat(), getSpeedIV()));
+        // Physical
+        setDefenseValue(calculateStatValue(getDefenseStat(1), getPhysicalDefenseIV()), 1);
+        setAttackValue(calculateStatValue(getAttackStat(1), getPhysicalAttackIV()), 1);
+        // Special
+        setDefenseValue(calculateStatValue(getDefenseStat(2), getSpecialDefenseIV()), 2);
+        setAttackValue(calculateStatValue(getAttackStat(2), getSpecialAttackIV()), 2);
     }
 
-    public int getStatValue(int stat, int IV, int isHealth) {
+    public int calculateStatValue(int stat, int IV, int isHealth) {
         return (((IV + 2 * stat) * level / 100) + 10 + level);
     }
 
-    public int getStatValue(int stat, int IV) {
+    public int calculateStatValue(int stat, int IV) {
         return (((IV + 2 * stat) * level / 100) + 5); // * Nature Value
-    }
-
-    public int getHealthValue() {
-        return this.getStatValue(health, healthIV, 1);
-    }
-
-    public int getPhysicalAttackValue() {
-        return this.getStatValue(physicalAttack, physicalAttackIV);
-    }
-
-    public int getSpecialAttackValue() {
-        return this.getStatValue(specialAttack, specialAttackIV);
-    }
-
-    public int getPhysicalDefenseValue() {
-        return this.getStatValue(physicalDefense, physicalDefenseIV);
-    }
-
-    public int getSpecialDefenseValue() {
-        return this.getStatValue(specialDefense, specialDefenseIV);
-    }
-
-    public int getSpeedValue() {
-        return this.getStatValue(speed, speedIV);
     }
 
     public void createPokemon(String name, Moves[] moves) {
@@ -99,11 +72,173 @@ public class Pokemon {
         return this;
     }
 
+    // Nome
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    // Nível
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    // Health
+    public int getHealthValue() {
+        return this.health[0];
+    }
+
+    public void setHealthValue(int health) {
+        this.health[0] = health;
+    }
+
+    public int getHealthIV() {
+        return this.health[2];
+    }
+
+    private void setHealthIV(int healthIV) {
+        this.health[2] = healthIV;
+    }
+
+    public int getHealthStat() {
+        return this.health[2];
+    }
+
+    public void setHealthStat(int healthStat) {
+        this.health[3] = healthStat;
+    }
+
+    // Speed
+    public int getSpeedValue() {
+        return this.speed[0];
+    }
+
+    public void setSpeedValue(int speed) {
+        this.speed[0] = speed;
+    }
+
+    public int getSpeedIV() {
+        return this.speed[2];
+    }
+
+    private void setSpeedIV(int speedIV) {
+        this.speed[2] = speedIV;
+    }
+
+    public int getSpeedStat() {
+        return this.speed[2];
+    }
+
+    public void setSpeedStat(int speedStat) {
+        this.speed[3] = speedStat;
+    }
+
+    // Defense
+    public int getDefenseValue(int type) {
+        if (type == 1) { //Fisico
+            return this.physicalDefense[0];
+        } else {
+            return this.specialDefense[0];
+        }
+    }
+
+    public void setDefenseValue(int defense, int type) {
+        if (type == 1) { //Fisico
+            this.physicalDefense[0] = defense;
+        } else {
+            this.specialDefense[0] = defense;
+        }
+    }
+
+    public int getDefenseStat(int type) {
+        if (type == 1) { // Fisico
+            return this.physicalDefense[2];
+        } else { // Fisico
+            return this.specialDefense[2];
+        }
+    }
+
+    public void setDefenseStat(int defense, int type) {
+        if (type == 1) { // Fisico
+            this.physicalDefense[3] = defense;
+        } else {
+            this.specialDefense[3] = defense;
+        }
+    }
+
+    // PhysicalDefense
+    public int getPhysicalDefenseIV() {
+        return this.physicalDefense[2];
+    }
+
+    private void setPhysicalDefenseIV(int physicalDefenseIV) {
+        this.physicalDefense[2] = physicalDefenseIV;
+    }
+
+    // SpecialDefense
+    public int getSpecialDefenseIV() {
+        return this.specialDefense[2];
+    }
+
+    private void setSpecialDefenseIV(int specialDefenseIV) {
+        this.specialDefense[2] = specialDefenseIV;
+    }
+
+    // Attack
+    public int getAttackValue(int type) {
+        if (type == 1) { //Fisico
+            return this.physicalAttack[0];
+        } else {
+            return this.specialAttack[0];
+        }
+    }
+
+    public void setAttackValue(int attack, int type) {
+        if (type == 1) { //Fisico
+            this.physicalAttack[0] = attack;
+        } else {
+            this.specialAttack[0] = attack;
+        }
+    }
+
+    public int getAttackStat(int type) {
+        if (type == 1) { // Fisico
+            return this.physicalAttack[2];
+        } else { // Fisico
+            return this.specialAttack[2];
+        }
+    }
+
+    public void setAttackStat(int attack, int type) {
+        if (type == 1) { // Fisico
+            this.physicalAttack[3] = attack;
+        } else {
+            this.specialAttack[3] = attack;
+        }
+    }
+
+    // PhysicalAttack
+    public int getPhysicalAttackIV() {
+        return this.physicalAttack[2];
+    }
+
+    private void setPhysicalAttackIV(int physicalAttackIV) {
+        this.physicalAttack[2] = physicalAttackIV;
+    }
+
+    // SpecialAttack
+    public int getSpecialAttackIV() {
+        return this.specialAttack[2];
+    }
+
+    private void setSpecialAttackIV(int specialAttackIV) {
+        this.specialAttack[2] = specialAttackIV;
     }
 }
